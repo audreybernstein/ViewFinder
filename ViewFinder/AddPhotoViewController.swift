b9283715dee249ce6165810657c08eef06f614cd
Apple <apple@Apples-MacBook-Pro-16.local>
2019-07-30 09:02:25 -0700

    Final iteration 2

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AddPhotoViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+
     var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +29,36 @@ class AddPhotoViewController: UIViewController,UIImagePickerControllerDelegate, 
         present(imagePicker, animated : true, completion : nil)
     }
     @IBOutlet weak var newImage: UIImageView!
-    private func imagePickerController( picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any] ) {
+    internal func imagePickerController(_  picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any] ) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         {
-        newImage.image = selectedImage }
+        newImageView.image = selectedImage }
     imagePicker.dismiss(animated : true, completion : nil)
     }
     
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            
+            photoToSave.caption = captionText.text
+            
+            if let userImage = newImageView.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                navigationController?.popViewController(animated: true)
+            }
+    
+        }
+    }
+    
+    
+    @IBOutlet weak var captionText: UITextField!
+    @IBOutlet weak var newImageView: UIImageView!
 }
     
 
@@ -47,4 +71,5 @@ class AddPhotoViewController: UIViewController,UIImagePickerControllerDelegate, 
         // Pass the selected object to the new view controller.
     }
     */
+
 
